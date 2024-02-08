c28b6a40bbefe23f74b2ef79131e9dd95a8691d8
Catherina Balaa <catherina.balaa@hotmail.com>
2024-02-08 09:36:48 +0200

    Added click event to heroes list in heroes component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,10 +9,15 @@ import { NgFor } from '@angular/common';
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id:1,
-    name:'Windstorm'
-  };
+  // hero: Hero = {
+  //   id:1,
+  //   name:'Windstorm'
+  // };
 
   heroes=HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero=hero;
+  }
 }
