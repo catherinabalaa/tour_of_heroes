f324490f3185139786f3f5d3839cd9ae8f735214
Catherina Balaa <catherina.balaa@hotmail.com>
2024-02-10 18:04:27 +0200

    Extracted the id route parameter in hero detail component and added getHero method in hero service

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -18,4 +18,13 @@ export class HeroDetailComponent {
     private location: Location, 
     private heroService: HeroService
     ) {}
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id=Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id).subscribe(hero => this.hero=hero);
+  }
 }
