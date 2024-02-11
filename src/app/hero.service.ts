06fdd18f3e7a31d014ac3d1a9d040db76e09b248
Catherina Balaa <catherina.balaa@hotmail.com>
2024-02-11 14:13:09 +0200

    Modified heroes service to import http client and http headers and added a url to web api

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,13 +3,18 @@ import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes'; //URL to web api
+
+  constructor(
+    private messageService: MessageService, 
+    private http: HttpClient) { }
 
   getHeroes():Observable<Hero[]> {
     const heroes=of(HEROES);
@@ -21,5 +26,9 @@ export class HeroService {
     const hero=HEROES.find(h=>h.id===id)!;
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(hero);
+  }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
